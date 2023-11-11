f3a821bbefacd1b7fe6c47c4dd94d9df08a6f666
chubbyle <59113939+chubbyle@users.noreply.github.com>
2023-11-11 18:56:04 +0800

    Added current day/time functionality

--- src/index.js
+++ src/index.js
@@ -6,13 +6,41 @@ function refreshWeatherData (response) {
     let windSpeedElement = document.querySelector("#wind-speed");
     let humidityElement = document.querySelector("#humidity");
 
+    let timeElement = document.querySelector("#time");
+    let date = new Date(response.data.time * 1000);
+
     cityElement.innerHTML = response.data.city;
     temperatureElement.innerHTML = Math.round(temperature);
     descriptionElement.innerHTML = response.data.condition.description;
     windSpeedElement.innerHTML = `${response.data.wind.speed
 } km/h`;
     humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+
+    timeElement.innerHTML = formatDate(date);
+    
 }
+
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day} ${hours}:${minutes} `;
+}
+
 function searchCity (city) {
     let apiKey = "d43e706ted81e54bf1d00fo1a187a1b5";
     let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
