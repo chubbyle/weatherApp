20b7a4e27e35ed5e478a2d2293b1bc487a03502d
chubbyle <59113939+chubbyle@users.noreply.github.com>
2023-11-11 17:58:41 +0800

    Fixed weather description, wind speed and humidity behavior

--- src/index.js
+++ src/index.js
@@ -2,12 +2,17 @@ function refreshWeatherData (response) {
     let temperatureElement = document.querySelector("#temperature");
     let temperature = response.data.temperature.current;
     let cityElement = document.querySelector("h1");
-
+    let descriptionElement = document.querySelector("#description");
+    let windSpeedElement = document.querySelector("#wind-speed");
+    let humidityElement = document.querySelector("#humidity");
 
     cityElement.innerHTML = response.data.city;
     temperatureElement.innerHTML = Math.round(temperature);
+    descriptionElement.innerHTML = response.data.condition.description;
+    windSpeedElement.innerHTML = `${response.data.wind.speed
+} km/h`;
+    humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
 }
-
 function searchCity (city) {
     let apiKey = "d43e706ted81e54bf1d00fo1a187a1b5";
     let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
